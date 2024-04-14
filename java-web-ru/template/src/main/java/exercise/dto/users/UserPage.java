5a77f973daf1f4faf50b596d90588c51c3bd6d05
@hexlet/cli <support@hexlet.io>
2024-04-14 11:58:38 +0300

    submit java-web-ru/template

---
+++ java-web-ru/template/src/main/java/exercise/dto/users/UserPage.java
@@ -0,0 +1,18 @@
+package exercise.dto.users;
+
+import exercise.model.User;
+
+
+// BEGIN
+public class UserPage {
+    private User user;
+
+    public UserPage(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
+// END
