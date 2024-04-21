8699eb0328f130936a06ca08cd8e3b3687434352
@hexlet/cli <support@hexlet.io>
2024-04-21 10:48:57 +0300

    submit java-spring-ru/jpa-data

---
+++ java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -0,0 +1,28 @@
+package exercise.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+
+import static jakarta.persistence.GenerationType.AUTO;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Entity
+@Table(name = "persons")
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Person {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+}
+// END
---
+++ java-spring-ru/jpa-data/src/main/java/exercise/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import exercise.model.Person;
+
+// BEGIN
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
+
+// END
